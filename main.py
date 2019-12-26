63e8f599e70eaf90c77a812153a8eeb355b10dae
Avi Milner <avim2809@gmail.com>
2019-12-26 14:33:05 +0200

    working app - blocks dns from cam app using shared dns database dict and thread lock mechanism

---
+++ main.py
@@ -0,0 +1,14 @@
+from dnschef.dnschef import start_dns_server
+from gad import start_cam_therad
+from threading import Lock
+
+blocked_domains = {'avi.org':'1.1.1.1'}  #TODO this should be updated from GUI,beacuse everything if passed by reference
+# we can update this from any place and it will affect all apps
+dns_lock = Lock()  # shared lock between cam and dns apps
+dns_db = start_dns_server("127.0.0.1",blocked_urls=blocked_domains,lock_dns=dns_lock)
+# dns db is the dict that both apps will use
+start_cam_therad(dns_db=dns_db,dns_lock=dns_lock,blocked_urls=blocked_domains)
+
+while True:
+    pass
+
